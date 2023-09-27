f45f9f4fa6704e01fd89f3473b2d88f279b375cb
3bdalah <3bdallah.dev@gmail.com>
2023-09-27 05:19:30 +0300

    feat(movies): - Service: Fetch all popular movies - Component: Display all movies in the movies list - Component: Create the movie card structure - Routing: Update routing to navigate to the movie details page - Component: Get full data for a movie in the details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-
+import { CommonModule } from '@angular/common';
+import { MoviesModule } from './movies/movies.module';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    SharedModule,AppRoutingModule
+    SharedModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    MoviesModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MoviesInterface } from '../movies-interface';
+import { MoviesService } from '../movies.service';
 
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
-  styleUrls: ['./movies-list.component.css']
+  styleUrls: ['./movies-list.component.css'],
 })
 export class MoviesListComponent {
-
+  listMovies: MoviesInterface[] = [];
+  fullyDataMovies: any = [];
+  constructor(private moviesServ: MoviesService) {}
+  ngOnInit() {
+    this.moviesServ.getAllMovies().subscribe((movie) => {
+      console.log('movie', movie.results);
+      let moviesTemp = movie;
+      this.listMovies = moviesTemp.results;
+    });
+  }
 }
