b1749272c2e57379480d8514e9646c13beeaa03b
MahmoudAlsameen <mahmoud_abdulrahman1@outlook.com>
2023-09-28 02:42:27 +0300

    checked

--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -7,6 +7,8 @@ export class MoviesService {
   private api_Key = 'fd1abe0fb535494df40c06c72320b913';
   private URLApi = 'https://api.themoviedb.org/3/movie/popular?api_key=';
   private MovieDetailesApi = 'https://api.themoviedb.org/3/movie/';
+  // "https://api.themoviedb.org/3/movie/565770/recommendations?api_key=fd1abe0fb535494df40c06c72320b913"
+  // private recommendationsMovie = '565770/recommendations?api_key=fd1abe0fb535494df40c06c72320b913'
   constructor(private http: HttpClient) {}
 
   getAllMovies() {
@@ -16,6 +18,14 @@ export class MoviesService {
   getfullyDataMovie(movieID: number) {
     return this.http.get<any>(
       this.MovieDetailesApi + movieID + '?api_key=' + this.api_Key
+    );
+  }
+  getAllRecommendationMovies(movieID: number) {
+    return this.http.get<any>(
+      this.MovieDetailesApi +
+        movieID +
+        '/recommendations?api_key=' +
+        this.api_Key
     );
   }
 }
