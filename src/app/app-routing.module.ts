df70ab05ba2170d61300a4e1a5a22c0304d2a0b2
MahmoudAlsameen <mahmoud_abdulrahman1@outlook.com>
2023-09-28 02:12:19 +0300

    checked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,19 @@ import { WishListComponent } from './watch-list/wish-list/wish-list.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 const routes: Routes = [
-  {path:'',component:MoviesListComponent},
-  {path:'watch-list',component:WishListComponent},
-  {path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent},
-  {path:'**',component:NotFoundComponent}
+  { path: '', component: MoviesListComponent },
+  { path: 'watch-list', component: WishListComponent },
+  { path: 'movie/:id', component: MovieDetailsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
